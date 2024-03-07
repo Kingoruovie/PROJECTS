c56f91687124490335865bd65d1321e61e8391ec
Oru Ovie <oruovie@gmail.com>
2024-03-07 10:10:53 +0100

    Made the newsletter design using the mobile first approach
    Using this approach seems easier than using the desktop first approach.
    Anyways, I had to learn about the hamburger issue of resizing the screen
    and how the links underneath does not fit in. I bypassed this using the
    event listener of resize to the window to set the width of the nav based
    on the window size.

---
+++ FRONTEND/news-homepage-main/index.js
@@ -0,0 +1,24 @@
+const hamBtn = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const closeBtn = document.querySelector(".close")
+const body = document.querySelector("body")
+
+
+hamBtn.addEventListener("click", function() {
+    nav.style.width = "18rem"
+    body.style.backgroundColor = "hsla(0, 0%, 0%, 0.4)"
+})
+
+closeBtn.addEventListener("click", function() {
+    nav.style.width = "0"
+    body.style.backgroundColor = "hsl(36, 100%, 99%)"
+})
+
+window.addEventListener("resize", function() {
+    if (Number(window.innerWidth) > 750) {
+        nav.style.width = "auto"   
+    } else {
+        nav.style.width = "0"    
+    }
+})
+
