c4810d2cae3efba25a8b225dc41810ee9212f2bd
Oru Ovie <oruovie@gmail.com>
2024-04-08 10:52:34 +0100

    Add the Interactive feature for the pricing card
    While the feature I stumbled making a drag and move effect for an
    element, just basically trying to simulate a slider. I later came to
    use the slider element for accessibilty reasons where I later later
    learnt how to style it differently using the webkit apperance and moz
    appearance. However, I was able to restrict the range element using js.
    Now, for the custom slider, I used mouse down and mouse up feature, although
    the mouseup feature was set on the document. Lastly, to add movement, I had
    to utilize a BrowserAPI called getBoundingClientRect which is simple words
    give the dimension and position of an element it is called upon.

--- FRONTEND/interactive-pricing-component-main/index.js
+++ FRONTEND/interactive-pricing-component-main/index.js
@@ -1,11 +1,25 @@
 const rangeInput = document.querySelector("input[type='range']");
 const checkInput = document.querySelector("input[type='checkbox']");
+const pageViews = document.querySelector("main > div > div p span:nth-child(1)")
+const pricing = document.querySelector("main > div > div div span:nth-child(1)")
+const pricingObj = {
+    "25": [8, "10k"],
+    "50": [16, "100k"],
+    "75": [24, "500k"],
+    "100": [32, "1m"]
+}
 let disocunt = false
 
 function makeRangeProgress(percent=50) {
-    rangeInput.classList.add("bg-gradient-to-r", "from-softCyan", `from-${percent}%`, "to-lightGrayishBlueEmpty", `to-${percent}%`)
+    const priceArr = pricingObj[`${percent}`]
+    let price = priceArr[0]
+    if (disocunt) {
+        price = priceArr[0] - priceArr[0] * 0.25
+    }
     rangeInput.value = percent
-    console.log(percent)
+    rangeInput.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${percent}%, hsl(224, 65%, 95%) ${percent}%)`
+    pageViews.textContent = priceArr[1]
+    pricing.textContent = `$${price}.00`
 }
 
 checkInput.addEventListener("input", () => {
@@ -17,8 +31,16 @@ checkInput.addEventListener("input", () => {
         checkInput.value = "false"
         disocunt = false
     }
-    console.log(disocunt)
+    makeRangeProgress(Number(rangeInput.value))
 })
 
 makeRangeProgress()
-rangeInput.addEventListener("input", (event) => makeRangeProgress(event.target.value))
+rangeInput.addEventListener("input", (event) => {
+    let percent = Number(event.target.value)
+    if (percent % 25 == 0 && percent == 0) {
+        percent = 25
+    } else if (percent % 25 != 0) {
+        percent = percent - (percent % 25) + 25
+    }
+    makeRangeProgress(percent)
+})
