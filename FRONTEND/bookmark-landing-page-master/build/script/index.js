fd776631edcf2963c1607e4216c73dc4f76a38a8
Oru Ovie <oruovie@gmail.com>
2025-02-09 23:15:24 +0100

    Add interactivity to the page

--- FRONTEND/bookmark-landing-page-master/build/script/index.js
+++ FRONTEND/bookmark-landing-page-master/build/script/index.js
@@ -2,9 +2,53 @@
 const hamburgerBtn = document.querySelector(".hamburger button");
 const closeBtn = document.querySelector("nav .close");
 const nav = document.querySelector("header nav");
+const tabBtns = document.querySelectorAll("#features div.tab-btns button");
+const accordionBtns = document.querySelectorAll(".accordion-item button");
+const email = document.querySelector("form input[type='email']");
+const form = document.querySelector("form");
+const formDiv = form === null || form === void 0 ? void 0 : form.querySelector("div");
+const span = form === null || form === void 0 ? void 0 : form.querySelector("span");
+const error = form === null || form === void 0 ? void 0 : form.querySelector("img");
 hamburgerBtn === null || hamburgerBtn === void 0 ? void 0 : hamburgerBtn.addEventListener("click", () => {
     nav === null || nav === void 0 ? void 0 : nav.classList.remove("translate-x-[100vw]");
 });
 closeBtn === null || closeBtn === void 0 ? void 0 : closeBtn.addEventListener("click", () => {
     nav === null || nav === void 0 ? void 0 : nav.classList.add("translate-x-[100vw]");
+});
+tabBtns.forEach(function (tabBtn) {
+    tabBtn.addEventListener("click", function () {
+        const id = tabBtn.id;
+        const activeTab = document.querySelector(".active-tab");
+        const tab = document.querySelector(`.${id}`);
+        const activeBtn = document.querySelector(".active-btn");
+        activeBtn === null || activeBtn === void 0 ? void 0 : activeBtn.classList.remove("active-btn");
+        tabBtn.classList.add("active-btn");
+        activeTab === null || activeTab === void 0 ? void 0 : activeTab.classList.replace("active-tab", "hidden");
+        tab === null || tab === void 0 ? void 0 : tab.classList.replace("hidden", "active-tab");
+    });
+});
+accordionBtns.forEach(function (accordionBtn) {
+    accordionBtn.addEventListener("click", function () {
+        var _a;
+        const accordion = (_a = accordionBtn.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode;
+        const answer = accordion === null || accordion === void 0 ? void 0 : accordion.querySelector(".answer");
+        answer === null || answer === void 0 ? void 0 : answer.classList.toggle("h-0");
+    });
+});
+function showError() {
+    if (email.validity.valueMissing || email.validity.typeMismatch) {
+        span === null || span === void 0 ? void 0 : span.classList.replace("hidden", "block");
+        error === null || error === void 0 ? void 0 : error.classList.remove("hidden");
+        formDiv === null || formDiv === void 0 ? void 0 : formDiv.classList.add("bg-soft-red");
+    }
+    else {
+        email.value = "";
+        span === null || span === void 0 ? void 0 : span.classList.replace("block", "hidden");
+        error === null || error === void 0 ? void 0 : error.classList.add("hidden");
+        formDiv === null || formDiv === void 0 ? void 0 : formDiv.classList.remove("bg-soft-red");
+    }
+}
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    showError();
 });
--- FRONTEND/bookmark-landing-page-master/src/script/index.ts
+++ FRONTEND/bookmark-landing-page-master/src/script/index.ts
@@ -1,6 +1,14 @@
 const hamburgerBtn = document.querySelector(".hamburger button");
 const closeBtn = document.querySelector("nav .close");
 const nav = document.querySelector("header nav");
+const tabBtns = document.querySelectorAll("#features div.tab-btns button");
+const accordionBtns = document.querySelectorAll(".accordion-item button");
+const email = document.querySelector("form input[type='email']") as HTMLInputElement;
+const form = document.querySelector("form");
+const formDiv = form?.querySelector("div")
+const span = form?.querySelector("span")
+const error = form?.querySelector("img")
+
 
 hamburgerBtn?.addEventListener("click", () => {
   nav?.classList.remove("translate-x-[100vw]");
@@ -10,4 +18,41 @@ closeBtn?.addEventListener("click", () => {
   nav?.classList.add("translate-x-[100vw]")
 })
 
+tabBtns.forEach(function(tabBtn) {
+  tabBtn.addEventListener("click", function() {
+    const id = tabBtn.id;
+    const activeTab = document.querySelector(".active-tab");
+    const tab = document.querySelector(`.${id}`);
+    const activeBtn = document.querySelector(".active-btn");
+    activeBtn?.classList.remove("active-btn");
+    tabBtn.classList.add("active-btn");
+    activeTab?.classList.replace("active-tab", "hidden")
+    tab?.classList.replace("hidden", "active-tab")
+  })
+})
 
+accordionBtns.forEach(function(accordionBtn) {
+  accordionBtn.addEventListener("click", function() {
+    const accordion = accordionBtn.parentNode?.parentNode
+    const answer = accordion?.querySelector(".answer");
+    answer?.classList.toggle("h-0");
+  })
+})
+
+function showError() {
+  if (email.validity.valueMissing || email.validity.typeMismatch) {
+    span?.classList.replace("hidden", "block");
+    error?.classList.remove("hidden");
+    formDiv?.classList.add("bg-soft-red")
+  } else {
+    email.value = ""
+    span?.classList.replace("block", "hidden");
+    error?.classList.add("hidden");
+    formDiv?.classList.remove("bg-soft-red")
+  }
+}
+
+form?.addEventListener("submit", function(event) {
+  event.preventDefault()
+  showError()
+})
